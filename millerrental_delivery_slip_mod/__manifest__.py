96436eecc1dfea8834834bbfb9677536da1d641f
Clay Sorrick <cso@odoo.com>
2018-07-25 14:08:04 -0700

    [ADD] millerrental_delivery_slip_mod: Added the rental agreement and notices text block to the end of the delivery slip for only Miller Rentals Inc. Added 4 boxes under the agreement for signatures upon delivery.

---
+++ millerrental_delivery_slip_mod/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "millerrental_delivery_slip_mod",
+
+    'summary': "Adding agreement/signature to delivery slip",
+
+    'description': "Adding an agreement and signature section to the Delivery Slip Report",
+
+    'author': "odoo, inc",
+    'website': "http://www.odoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['stock', 'web'],
+
+    # always loaded
+    'data': [
+        'report/custom_assets.xml',
+        'report/delivery_slip_mod.xml',
+    ],
+    'auto_install': True,
+}
